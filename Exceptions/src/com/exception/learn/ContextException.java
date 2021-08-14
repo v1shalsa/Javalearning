459d90b63c8ff110851d98469294a8f2520474d1
v1shalsa <salvi.vishal@gmail.com>
2021-08-14 15:54:22 +0530

    trial

--- Exceptions/src/com/exception/learn/ContextException.java
+++ Exceptions/src/com/exception/learn/ContextException.java
@@ -3,7 +3,7 @@ package com.exception.learn;
 public class ContextException extends Exception {
 	
 	public void exceptionExample(int someVar,int someOtherVar) throws ContextException{
-		
+		//Checked Exception learning
 		int total = someVar + someOtherVar;
 		if(someVar > someOtherVar) throw new ContextException();
 		System.out.println("Total = "+someVar+someOtherVar);				
